856eba66694e0325a08c1d8dab0ad1caea02aaa0
vxtien <vxtien1012@gmail.com>
2023-03-21 11:08:27 +0700

    update

---
+++ app.js
@@ -0,0 +1,507 @@
+let employ = {
+    name: "Nguyễn Duy",
+    age: 25,
+    salary: 1000,
+    hometown: "Hà Nội",
+    job:"Kỹ sư phần cứng",
+    level: "junior",
+    maritalStatus:"married",
+    children: 0,
+    workplace: "Hà Nội"
+};
+
+let employs = [
+    {
+        name: "Nguyễn Duy A",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy B",
+        age: 25,
+        salary: 1000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy C",
+        age: 25,
+        salary: 1000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy D",
+        age: 25,
+        salary: 1000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy E",
+        age: 25,
+        salary: 1000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy F",
+        age: 25,
+        salary: 1000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy G",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy H",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy J",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy K",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy L",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy M",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy N",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy Q",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy Ư",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy W",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy R",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy T",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy Y",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy U",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy O",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy P",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy S",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy Z",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy X",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy V",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 1",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 2",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 3",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 4",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 5",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 6",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 7",
+        age: 25,
+        salary: 25000000,
+        hometown: "HCM",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 8",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 8",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 9",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 10",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 11",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 12",
+        age: 25,
+        salary: 25000000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Married",
+        children:2,
+        workplace: "Hà Nội"
+    },
+    {
+        name: "Nguyễn Duy 13",
+        age: 25,
+        salary: 1000,
+        hometown: "Hà Nội",
+        job:"Kỹ sư phần cứng",
+        level: "junior",
+        maritalStatus:"Not married",
+        children: 0,
+        workplace: "Hà Nội"
+    },
+];
+
+
+const employeeTable = document.getElementById('employeeTable');
+
+employs.map(employ => {
+  const tr = document.createElement('tr');
+
+  const nameTd = document.createElement('td');
+  nameTd.appendChild(document.createTextNode(employ.name));
+  tr.appendChild(nameTd);
+
+  const ageTd = document.createElement('td');
+  ageTd.appendChild(document.createTextNode(employ.age));
+  tr.appendChild(ageTd);
+
+  const salaryTd = document.createElement('td');
+  salaryTd.appendChild(document.createTextNode(employ.salary));
+  tr.appendChild(salaryTd);
+
+  const hometownTd = document.createElement('td');
+  hometownTd.appendChild(document.createTextNode(employ.hometown));
+  tr.appendChild(hometownTd);
+
+  const jobTd = document.createElement('td');
+  jobTd.appendChild(document.createTextNode(employ.job));
+  tr.appendChild(jobTd);
+
+  const levelTd = document.createElement('td');
+  levelTd.appendChild(document.createTextNode(employ.level));
+  tr.appendChild(levelTd);
+
+  const maritalStatusTd = document.createElement('td');
+  maritalStatusTd.appendChild(document.createTextNode(employ.maritalStatus));
+  tr.appendChild(maritalStatusTd);
+
+  const childrenTd = document.createElement('td');
+  childrenTd.appendChild(document.createTextNode(employ.children));
+  tr.appendChild(childrenTd);
+
+  const workplaceTd = document.createElement('td');
+  workplaceTd.appendChild(document.createTextNode(employ.workplace));
+  tr.appendChild(workplaceTd);
+
+  employeeTable.appendChild(tr);
+});
+
+
+let results = employs.filter((item)=> item.hometown == "Hà Nội" );
+console.log(results);
+
+let results1 = employs.filter((item)=> item.salary >=20000000 && item.children >= 2 )
+console.log(results1);
+
